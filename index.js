6e4df491955fcc9066dc9f4bbc76475f68ae3d55
Bryce Jech <bryce@brycejech.com>
2018-10-07 07:09:46 -0500

    mv signup-genius-client.js -> index.js

--- index.js
+++ index.js
@@ -1,65 +1,139 @@
 'use strict';
 
-const sgClient = require('./signup-genius-client');
-
-const apiKey  = require('./conf').apiKey;
-
-const client = new sgClient(apiKey);
+const request = require('request');
 
 
-// Get some information about the account and a list
-// of signups with their registrants
+function SignupGeniusClient(apiKey){
+    if(!apiKey) throw new Error('apiKey required');
 
-(async () => {
+    this.baseUrl = 'https://api.signupgenius.com/v2/k';
+    this.apiKey  = apiKey;
 
-    const data = { };
+    return this;
+}
 
-    const user = await client.getProfile();
-
-    data.accountName  = `${ user.firstname } ${ user.lastname }`;
-    data.accountEmail = user.email;
-    data.accountID    = user.memberid;
-    data.signups      = [];
-
-    const signups = await client.getAllSignups();
-
-
-    const promises = signups.map(async (signup) => {
-
-        const obj = {
-            title:          signup.title,
-            id:             signup.signupid,
-            start:          new Date(signup.startdate * 1000),
-            end:            new Date(signup.enddate   * 1000),
-            url:            signup.signupurl,
-            totalSlots:     signup.slotmetrics.totalslots,
-            slotsAvailable: signup.slotmetrics.totalavailableslots,
-            slotsFilled:    signup.slotmetrics.totalslotsfilled,
-            registrants:    [ ]
+SignupGeniusClient.prototype.makeRequest = function makeRequest(url){
+    return new Promise((resolve, reject) => {
+        const opt = {
+            url: url,
+            method: 'GET',
+            json: true,
+            headers: {
+                'accept': 'application/json'
+            }
         }
-
-        const title = signup.title,
-              id    = signup.signupid,
-              start = new Date(signup.startdate * 1000);
-
-        const registrants = await client.getSignupRegistrants(id);
-
-        registrants.forEach(person => {
-            obj.registrants.push({
-                name: `${ person.firstname } ${ person.lastname }`,
-                email: person.email
-            })
+        request(opt, (err, response, body) => {
+            if(err) return reject(err);
+            resolve(body);
         });
-
-        data.signups.push(obj);
-
-        return new Promise((resolve, reject) => resolve(obj));
     });
+}
 
-    Promise.all(promises)
-        .then(() => {
-            console.log(JSON.stringify(data, null, 2))
-        })
-        .catch(e => console.log(e));
+SignupGeniusClient.prototype.getProfile = function getProfile(){
 
-})();
+    const url = `${ this.baseUrl }/user/profile/?user_key=${ this.apiKey }`;
+
+    return new Promise((resolve, reject) => {
+        this.makeRequest(url)
+            .then(res => {
+                if(res.success){
+                    resolve(res.data);
+                }
+                reject(new Error('Error fetching profile'));
+
+            })
+            .catch(e => {
+                reject(e);
+            });
+    });
+}
+
+SignupGeniusClient.prototype.getAllSignups = function getAllSignups(){
+
+    const url = `${ this.baseUrl }/signups/created/all/?user_key=${ this.apiKey }`;
+
+    return new Promise((resolve, reject) => {
+        this.makeRequest(url)
+            .then(res => {
+                if(res.success){
+                    resolve(res.data);
+                }
+                reject(new Error('Error fetching signups'));
+            })
+            .catch(e => {
+                reject(e);
+            })
+    });
+}
+
+SignupGeniusClient.prototype.getActiveSignups = function getActiveSignups(){
+
+    const url = `${ this.baseUrl }/signups/created/active/?user_key=${ this.apiKey }`;
+
+    return new Promise((resolve, reject) => {
+        this.makeRequest(url)
+            .then(res => {
+                if(res.success){
+                    resolve(res.data);
+                }
+                reject(new Error('Error fetching signups'));
+            })
+            .catch(e => {
+                reject(e);
+            })
+    });
+}
+
+SignupGeniusClient.prototype.getExpiredSignups = function getExpiredSignups(){
+
+    const url = `${ this.baseUrl }/signups/created/expired/?user_key=${ this.apiKey }`;
+
+    return new Promise((resolve, reject) => {
+        this.makeRequest(url)
+            .then(res => {
+                if(res.success){
+                    resolve(res.data);
+                }
+                reject(new Error('Error fetching signups'));
+            })
+            .catch(e => {
+                reject(e);
+            })
+    });
+}
+
+SignupGeniusClient.prototype.getSignupReport = function getSignupReport(signupID){
+
+    const url = `${ this.baseUrl }/signups/report/all/${ signupID }/?user_key=${ this.apiKey }`;
+
+    return new Promise((resolve, reject) => {
+        this.makeRequest(url)
+            .then(res => {
+                if(res.success){
+                    resolve(res.data);
+                }
+                reject(new Error(`Error fetching signup report for signupID ${ signupID }`));
+            })
+            .catch(e => reject(e));
+    });
+}
+
+SignupGeniusClient.prototype.getSignupRegistrants = function getSignupRegistrants(signupID){
+
+    const url = `${ this.baseUrl }/signups/report/filled/${ signupID }/?user_key=${ this.apiKey }`;
+
+    return new Promise((resolve, reject) => {
+        this.makeRequest(url)
+            .then(res => {
+                if(res.success){
+                    resolve(res.data.signup);
+                }
+                reject(new Error(`Error fetching signup filled report for signupID ${ signupID }`));
+            })
+            .catch(e => reject(e));
+    });
+}
+
+
+
+module.exports = SignupGeniusClient;
